4c14d63b650c29c3d8730985ed0234f86bee1b1f
Rauan Zholaman <rzholama@c2r6s8.42singapore.sg>
2025-07-03 18:37:21 +0800

    progress

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "ft_printf.h"
+/*
+size_t	wordcount(const char *str)
+{
+	int count;
+	int is_space;
+
+	count = 0;
+	is_space = 0;
+	while (*str)
+	{
+		if (*str != ' ' && is_space == 0)
+		{
+			count++;
+			is_space = 1;
+		}
+		else if (*str == ' ')
+			is_space = 0;
+		str++;
+	}
+	return (count);
+}
 
 size_t formatcount(const char *format)
 {
@@ -13,31 +34,39 @@ size_t formatcount(const char *format)
 			count++;
 		if (!ft_strncmp(format, "%s", 2))
 			count++;
+		if (!ft_strncmp(format, "%zu", 3))
+			count++;
 		format++;
 	}
 	return (count);
 }
 
-/*void	ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
 	va_list valist;
-	
 	va_start(valist, format);
-	
+
+	while 
 	char *str;
-	while ((str = va_arg(valist, char*)) != NULL)
-	{
-		printf(format, str);
-	}
-	if (valist == NULL)
-		valist 
-	va_end(valist);
+	str = va_arg(valist, char*);
 	
-}*/
+	size_t word_count;
+	size_t sign_count;
+
+	word_count = wordcount(format);
+	sign_count = formatcount(format);	
+	
+	printf("%lu", word_count);
+	//va_end(valist);
+}
+*/
 
 int main()
 {
-	//ft_printf("%s\n", "Hello,", "World!", "Of", "Destruction.", NULL);
+	//ft_printf("Idi Nahui! %s %s %d", "Hello, ", "World!", 123);
 
-	printf("%zu\n", formatcount("%d %d %s %s %s %zu"));
+	//printf("The return value is: %d", printf("Hello, World! %f", 1.2 + 1.32));
+	printf("%X\n", 123456789);
+	printf("%x\n", 123456789);
+
 }
